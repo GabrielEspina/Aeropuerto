f88340f647b1ff35a6f671cbf4b4b2a140a42cc4
WalterDuartesUB <37937023+WalterDuartesUB@users.noreply.github.com>
2018-04-24 23:19:23 -0300

    Se remueve la clase extra con el main

--- Trafico Aereo/src/Aplicacion.java
+++ Trafico Aereo/src/Aplicacion.java
@@ -1,3 +1,6 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import ar.edu.ub.p3.tpi.api.Avion;
 
 public class Aplicacion {
@@ -12,7 +15,32 @@ public class Aplicacion {
 		//en un while recibe mas de un aeropuerto
 		recepcion.start();
 		aterrizaje.start();
-
+/*
+Codigo que estaba en el otro main
+		int puerto = 8888;				//CREO UN PUERTO CUALQUIERA
+		Thread [] aeropuerto = new Thread[2];	//CREO DOS HILOS (DOS AEROPUERTOS PODRAN ENTRAR);
+		
+		Socket so;
+		for(int i=0;i<2;i++) {
+			try(ServerSocket ss = new ServerSocket(puerto)){
+				so = ss.accept();
+				System.out.println("Se conecto el aeropuerto " + so.getPort());
+				aeropuerto[i] = new Thread(new Conexion(so));
+			}catch(Exception e){
+				
+			}
+		}
+		
+		for(int i = 0 ; i<2; i++){
+			aeropuerto[i].start();
+		}
+		
+		for(int i = 0 ; i<2; i++){
+			aeropuerto[i].join();;
+		}
+		  
+ 
+ */
 	}
 
 }
